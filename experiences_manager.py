41a1d8c1568dc0126a0d53ca4aad06fcdb7b639b
Vendrell <davidvendrell6@gmail.com>
2023-12-17 02:46:22 +0100

    init of experiences_manager

--- experiences_manager.py
+++ experiences_manager.py
@@ -1,9 +1,26 @@
+import csv
+import json
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, KeyboardButton
+from sender import Sender
+
+
 
 class Experiences:
 
     def __init__(self):
-        pass
+        self.sender = Sender()
 
     def handle_button(self, user, content):
         if content == "exp_start":
-            pass
+            experiences = json.load(open("questionari.json"))
+
+            experiences = json.load(open("questionari.json"))
+
+            for i in range(20):
+                print(experiences[str(i+1)])
+
+                self.sender.send_message(user, experiences[str(i+1)]["question"], Update)
+
+
+            print(experiences)
